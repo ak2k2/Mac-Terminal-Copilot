2e56ff74ffcd09f165784f1b34abc7fccedc1ab1
ak2k2 <armaan.kapoor@cooper.edu>
2023-02-21 23:11:55 -0500

    clean

--- dumm/more/deeper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# echo "Hello World" and display the current date and time
-
-echo "Hello World"
-date
--- firsttest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Command Suggestions for a Mac bash terminal.
-
-# print all the files in all the directories in the current directory
-find . -type f
